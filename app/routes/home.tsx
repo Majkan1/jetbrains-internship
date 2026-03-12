9380857d5a09ce686b55066307b28d8c093da477
Mikołaj Michalak <majkanart117@gmail.com>
2026-03-12 12:03:55 +0100

    Change to basics UI/UX .

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,22 +1,16 @@
-import Header from "../welcome/components/Header";
-import Footer from "../welcome/components/Footer";
-
 export default function Index() {
   return (
-    <div style={{ fontFamily: "sans-serif" }}>
-      <Header />
-      
-      <main style={{ padding: "100px 20px", textAlign: "center", minHeight: "60vh" }}>
-        <h1 style={{ fontSize: "3rem", fontWeight: "bold" }}>
-          A modern programming language <br /> 
-          that makes developers happier
-        </h1>
-        <p style={{ fontSize: "1.2rem", color: "#666" }}>
-          Mój nowy projekt w React Router 7!
-        </p>
-      </main>
-
-      <Footer />
+    <div style={{ 
+      minHeight: "100vh", 
+      backgroundColor: "#000000", 
+      color: "#ffffff",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "sans-serif",
+      margin: 0
+    }}>
+      <h1 style={{ fontSize: "3rem" }}>hello programers</h1>
     </div>
   );
 }
